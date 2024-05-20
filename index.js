ede43448ba7fd57da14630a8cc90d41a4f69e287
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-05-20 20:05:55 +0530

    assign needed elements for dom manipulation

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.querySelector('.taskInput');
+    const timeInput = document.querySelector('.timeInput');
+    const addButton = document.querySelector('.addButton');
+    const toDoList = document.querySelector('.toDoList');
+    const completedList = document.querySelector('.completedList');
+    const urgencySelect = document.querySelector('.urgencySelect');
+    const unCompletedTask = document.querySelector('.unCompletedTask');
+    const completedTask = document.querySelector('.completedTask')
+    const selectedMenu = document.querySelectorAll('.selectedMenu');
+
+});
